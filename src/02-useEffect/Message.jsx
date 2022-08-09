4f9bb29721e9ffb032574ca8a77d3967b848c436
Rolando <rolandolazaro@gmail.com>
2022-08-09 11:01:24 -0500

    Se agrega la limpieza del componente addEventListener

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,18 +1,35 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export const Message = () => {
 
-    useEffect(() => {
-        console.log('Message Mounted');
+    const [coords, setCoords] = useState({x: 0, y: 0});
+
+    useEffect(() => {        
+        //Se monta el componente
+        //console.log('Message Mounted');
+        //Escuchamos los movimientos del mouse
+        const onMouseMove = ({x, y}) => {
+            //const coords = {x, y};
+            //console.log(coords);
+            setCoords({x, y});
+        }
+
+        window.addEventListener( 'mousemove', onMouseMove );
 
         return () => {
-            console.log('Message Unmounted');
+
+            //Se desmonta el componente
+            //console.log('Message Unmounted');
+            //Removemos el addEventListener
+            window.removeEventListener( 'mousemove', onMouseMove );
+
         }
     }, []);
 
     return (
         <>
             <h3>Usuario ya existe</h3>
+            { JSON.stringify( coords) }
         </>
     )
 }
