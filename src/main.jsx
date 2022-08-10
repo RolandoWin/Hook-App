36e9c05101320f4a8c167993a0572506452be4f7
Rolando <rolandolazaro@gmail.com>
2022-08-09 21:02:30 -0500

    Se agrega el Hook useForm

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { SimpleForm } from './02-useEffect/SimpleForm';
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+//import { SimpleForm } from './02-useEffect/SimpleForm';
 //import { CounterApp } from './01-useState/CounterApp';
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 //import { HooksApp } from './HooksApp';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SimpleForm />
-  </React.StrictMode>
+  //<React.StrictMode>
+    <FormWithCustomHook />
+  //</React.StrictMode>
 )
