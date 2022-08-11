0322b17ec0db45f4059485ce7858d7177ceee1aa
Rolando <rolandolazaro@gmail.com>
2022-08-11 10:33:39 -0500

    Se agrega el hook useLayoutEffect

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
-import { FocusScreen } from './04-useRef/FocusScreen';
+//import { FocusScreen } from './04-useRef/FocusScreen';
+import { Layout } from './05-useLayoutEffect/Layout';
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 //import { SimpleForm } from './02-useEffect/SimpleForm';
 //import { CounterApp } from './01-useState/CounterApp';
@@ -12,6 +13,6 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <FocusScreen />
+    <Layout />
   //</React.StrictMode>
 )
