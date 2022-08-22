2db5567154ae53bea690d886739075078fe03da6
Rolando <rolandolazaro@gmail.com>
2022-08-22 13:22:01 -0500

    Agregando test de pruebas al  MultipleCustomHokks

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -18,7 +18,7 @@ export const MultipleCustomHooks = () => {
 
     return (
         <>
-            <h1> Breaking Bad Quotes </h1>
+            <h1> BreakingBad Quotes </h1>
             <hr />
             {
                 // isLoading 
